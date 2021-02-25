5d9a2becb74c27534601352ffb6b5c976c0e2959
Jeyavaradhan K.S <jeyavardhanjai@gmail.com>
2021-02-25 11:45:02 +0530

    keywords testing

--- grammarChannel.py
+++ grammarChannel.py
@@ -172,5 +172,6 @@ def GrammerChannel(listwords):
 def SelectorVaraintHandler(input):
     variant = {
         ['camel','kamel'] : 'Camel',
+        ['args','arcs','ox','herbs','ags']:'args',
     }
     return input # ! change later to non-default function after code defnittion
